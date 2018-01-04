415c2b4d87c30ea862e2a223de8aef97af05a90d
Enrique Gachuz <enriqueibilis@gmail.com>
2018-01-04 17:32:37 -0600

    agregado controlador de la vista screenAdd como "CreateTaskViewController"

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Enrique Gachuz on 04/01/18.
+//  Copyright © 2018 Enrique Gachuz. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet var taskNameField: UITextField!
+    @IBOutlet var importatnSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+    }
+
+
+}
