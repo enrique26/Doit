05ef17f7e954d5714805214d960d295275477430
Enrique Gachuz <enriqueibilis@gmail.com>
2018-01-04 17:01:54 -0600

    crear table view, poblar tabla con clases

---
+++ Doit/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Enrique Gachuz on 04/01/18.
+//  Copyright © 2018 Enrique Gachuz. All rights reserved.
+//
+
+import Foundation
+
+
+class Task{
+    var name=""
+    var importat = false
+    
+}
--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet var tableViewOne: UITableView!
+    var tasks:[Task] = [];
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //first calle the function before create tableview
+        tasks=makeTask();
+        
+        tableViewOne.dataSource=self;
+        tableViewOne.delegate=self;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=UITableViewCell()
+        let task=tasks[indexPath.row];
+        if(task.importat==true){
+            cell.textLabel?.text="! \(task.name)"
+        }else{
+            cell.textLabel?.text=task.name
+        }
+        
+        return cell;!
     }
 
-
+    func makeTask()->[Task]{
+        let task1=Task();
+        task1.name="walk";
+        task1.importat=false;
+        
+        let task2=Task();
+        task2.name="walk 2";
+        task2.importat=false;
+        
+        let task3=Task();
+        task3.name="buy";
+        task3.importat=true;
+        
+        return [task1,task2,task3]
+    }
 }
 
